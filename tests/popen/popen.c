e6558eb46e29d064961b277ffc8ca1a525b82dda
Liunx <liunx163@163.com>
2014-12-13 17:57:58 +0800

    add popen tests

---
+++ tests/popen/popen.c
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  popen.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年12月13日 16时00分21秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/select.h>
+
+int main()
+{
+	FILE *fp;
+	int fd;
+	int flags = 1;
+	char buf[64];
+	ssize_t ret;
+	fd_set rfds;
+	printf("popen...\n");
+	fp = popen("sleep 10;echo hello", "r");
+	fd = fileno(fp);
+	FD_ZERO(&rfds);
+	FD_SET(fd, &rfds);
+	if (0) {
+		flags = fcntl(fd, F_GETFL, 0);
+		fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	}
+	printf("select...\n");
+	select(fd + 1, &rfds, NULL, NULL, NULL);
+	printf("fread...\n");
+	fread(buf, sizeof(char), 64, fp);
+	printf("pclose...\n");
+	pclose(fp);
+	printf("return...\n");
+	return 0;
+}
+
